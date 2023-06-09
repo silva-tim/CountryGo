17020ce860d7f3d8386ce49037bddb121396a152
Tim Silva <tim17silva@gmail.com>
2023-06-09 17:21:33 +0000

    Added function to send a XHR and save the response to array 'countries'. #1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let countries = null;
+
+function sendXHR() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://restcountries.com/v3.1/all');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    countries = xhr.response;
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
+  xhr.send();
+}
